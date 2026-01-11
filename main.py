00c125b48c9cd6e2112c7d3cc03cfdd621254f38
garishtinna123-cell <garishtinna123@gmail.com>
2026-01-11 18:13:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1. Create a variable named pi and check its data type
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+print("----------------------------")
+
+# 2. Try creating a variable named 'for'
+# for = 4   # ❌ This line will cause a SyntaxError because 'for' is a keyword
+
+# Correct way
+for_value = 4
+print("Value of for_value:", for_value)
+
+print("----------------------------")
+
+# 3. Calculate Simple Interest
+P = 5000   # Principal amount
+R = 5      # Rate of interest
+T = 3      # Time in years
+
+SI = (P * R * T) / 100
+print("Simple Interest for 3 years:", SI)
